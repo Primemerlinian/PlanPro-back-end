cb8d39b589bd9f0d33a969a132b91a8dbc0c88c2
Primemerlinian <mwatson0518@gmail.com>
2023-02-04 19:01:10 -0500

    fix category function

--- controllers/category.js
+++ controllers/category.js
@@ -1,15 +1,28 @@
 import { Category } from '../models/category.js'
 import { Profile } from '../models/profile.js'
 
-const newCategory = new Category({
-  title: req.body.title,
-  color:req.body.color
-});
+const createCategory = async (req, res) => {
+  try {
+  req.body.owner = req.user.profile
+  const category = await Category.create(req.body)
+  const profile = await Profile.findByIdAndUpdate(
+  req.user.profile,
+  { $push: { categories: category } },
+  { new: true }
+  )
+  category.owner = profile
+  res.status(201).json(category)
+  } catch (error) {
+  console.log(error)
+  res.status(500).json(error)
+  }
+  };
+
 
 
 
 
 export {
-  newCategory,
+  createCategory,
 
 }
